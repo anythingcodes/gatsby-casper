35dee01a1bf59616a3c685388aee50cc5e57e1b6
Liz Shaw <liz.h.shaw@gmail.com>
2020-12-26 09:43:21 -0500

    Add service worker and replace favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,45 @@ module.exports = {
         postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Liz Makes Things',
+        short_name: 'Liz Makes Things',
+        start_url: '/',
+        background_color: '#191b1f',
+        theme_color: '#fff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-offline',
     // {
     //   resolve: 'gatsby-plugin-google-analytics',
     //   options: {
