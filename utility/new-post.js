e3847abc9b582203678bba62f4b2058e8b0a6d92
Liz Shaw <liz.h.shaw@gmail.com>
2020-12-26 12:13:43 -0500

    Add new post utility script for ease-of-use

---
+++ utility/new-post.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const pathToFile = path.join(__dirname, 'new-post.md');
+const pathToNewDestination = path.join(__dirname, '../src/content', 'new.md');
+
+try {
+  fs.copyFileSync(pathToFile, pathToNewDestination);
+
+  fs.readFile(pathToNewDestination, 'utf8', (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    const result = data.replace(/\$DATE/g, new Date().toISOString());
+
+    fs.writeFile(pathToNewDestination, result, 'utf8', err => {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
+  console.log('Successfully copied and moved the file!');
+} catch (err) {
+  throw err;
+}
---
+++ utility/new-post.md
@@ -0,0 +1,16 @@
+---
+layout: post
+title: ''
+author: [Liz]
+tags: ['DIY']
+image: img/writing.jpg
+date: '$DATE'
+draft: false
+---
+
+Excerpt
+
+---
+
+# The content
+
