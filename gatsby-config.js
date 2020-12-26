8b3b69c72532bb5d0408a1aaaedb91e7cc58d64e
Liz Shaw <liz.h.shaw@gmail.com>
2020-12-25 19:13:04 -0500

    Change metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Casper',
-    description: 'A port of the casper blog built for gatsby',
+    title: 'Liz Makes Things',
+    description: 'DIY, software engineering, crafts, and all manner of constructable things.',
     siteUrl: 'https://www.liz.show', // full path to blog - no ending slash
   },
   mapping: {
@@ -43,7 +43,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 2000,
+              maxWidth: 1040,
               quality: 100,
             },
           },
@@ -69,23 +69,23 @@ module.exports = {
         postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
       },
     },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-XXXX-Y',
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // IP anonymization for GDPR compliance
-        anonymize: true,
-        // Disable analytics for users with `Do Not Track` enabled
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
-        // Specifies what percentage of users should be tracked
-        sampleRate: 100,
-        // Determines how often site speed tracking beacons will be sent
-        siteSpeedSampleRate: 10,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     trackingId: 'UA-XXXX-Y',
+    //     // Puts tracking script in the head instead of the body
+    //     head: true,
+    //     // IP anonymization for GDPR compliance
+    //     anonymize: true,
+    //     // Disable analytics for users with `Do Not Track` enabled
+    //     respectDNT: true,
+    //     // Avoids sending pageview hits from custom paths
+    //     exclude: ['/preview/**'],
+    //     // Specifies what percentage of users should be tracked
+    //     sampleRate: 100,
+    //     // Determines how often site speed tracking beacons will be sent
+    //     siteSpeedSampleRate: 10,
+    //   },
+    // },
   ],
 };
