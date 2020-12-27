af827b416c5776b9040ba6bdd12419218076e2e7
Liz Shaw <liz.h.shaw@gmail.com>
2020-12-26 22:03:03 -0500

    Move favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,27 +74,27 @@ module.exports = {
         display: 'standalone',
         icons: [
           {
-            src: '/android-chrome-192x192.png',
+            src: '/img/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/android-chrome-512x512.png',
+            src: '/img/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/apple-touch-icon.png',
+            src: '/img/apple-touch-icon.png',
             sizes: '180x180',
             type: 'image/png',
           },
           {
-            src: '/favicon-16x16.png',
+            src: '/img/favicon-16x16.png',
             sizes: '16x16',
             type: 'image/png',
           },
           {
-            src: '/favicon-32x32.png',
+            src: '/img/favicon-32x32.png',
             sizes: '32x32',
             type: 'image/png',
           },
